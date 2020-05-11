8c3088e3a45f5a3da517537fadd0264acf4088d6
Iurii.Zozulynskyi <iurii.zozulynskyi@gmail.com>
2020-05-12 00:36:46 +0400

    HW11 updated

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -54,9 +54,12 @@ const rootNode = document.getElementById('root');
 
 // TODO: your code goes here
 
-let folder = '<i class="material-icons">folder</i>';
+// Assign icons to variables 
+let folder = '<i class="material-icons folder">folder</i>';
 let icon = '<i class="material-icons file">insert_drive_file</i>';
-let openFolderIcon = '<i class="material-icons">folder_open</i>';
+let openFolderIcon = '<i class="material-icons folder">folder_open</i>';
+
+// Create a tree
 
 const createNode = (input) => {
   if(!input) {
@@ -64,7 +67,6 @@ const createNode = (input) => {
   }
 
   let ul = document.createElement('ul');
-  ul.classList.add('invisible');
 
   for (let obj of input) {
     let li = document.createElement('li');
@@ -87,32 +89,126 @@ const createNode = (input) => {
   return ul;
 };
 
-rootNode.append(createNode(data));
+rootNode.prepend(createNode(data));
 
-const files = document.getElementsByClassName('file');
-for (let file of files) {
-  file.style.color = 'lightgrey';
+for (let li of rootNode.querySelectorAll('li')) {
+  let div = document.createElement('div');
+  li.prepend(div);
+  div.append(div.nextSibling);
+  div.append(div.nextSibling);
 }
 
-rootNode.firstElementChild.className = 'list visible';
+// Hide all elements except top level of tree
 
-const changeBackground = (e) => {
-  if(e.type === 'mouseover') {
-    e.target.style.backgroundColor = 'lightgrey';
-  } else if (e.type === 'mouseout') {
-    e.target.style.backgroundColor = 'white';
+document.querySelectorAll('ul').forEach((elem) => {
+  if (elem.parentNode !== rootNode) {
+    elem.hidden = true;
   }
-}
+});
 
-const li = document.querySelectorAll('li')
-li .forEach(item => {
-  item.addEventListener('mouseover', changeBackground);
-  item.addEventListener('mouseout', changeBackground)
-})
+// Open folders onclick
 
-const ul = document.querySelectorAll('ul')
-ul.forEach(item => {
-  item.addEventListener('mouseover', changeBackground);
-  item.addEventListener('mouseout', changeBackground);
-})
+rootNode.onclick = function(event) {
+  if (event.target.tagName !== 'DIV') {
+    return;
+  }
+  if (event.target.firstElementChild.innerText === 'folder') {
+    event.target.firstElementChild.innerText = 'folder_open';
+  } else if (event.target.firstElementChild.innerText === 'folder_open') {
+    event.target.firstElementChild.innerText = 'folder';
+  }
+
+  let container = event.target.parentNode.querySelector('ul');
+  if (!container) {
+    if (event.target.firstElementChild.classList.contains('file')) {
+      return;
+    }
+    event.target.nextElementSibling.hidden = !event.target.nextElementSibling.hidden;   
+    return
+  }
+  container.hidden = !container.hidden;
+};
+
+const folders = document.querySelectorAll('.folder')
+
+folders.forEach((elem) => {
+  if(!elem.parentNode.nextElementSibling) {
+    let li = document.createElement('li');
+    let text = '<i class="cursor_text empty_folder">Folder is empty</i>';
+    li.innerHTML = text;
+    li.hidden = true;
+    elem.parentNode.after(li);
+  }
+});
+
+// Right-click-menu creation
+
+const createMenu = (options) => {
+  let ul = document.createElement('ul');
+  ul.classList.add('right-click-menu');
+
+  for (let i = 0; i < options.length; i += 1) {
+    let li = document.createElement('li');
+    li.classList.add(`opt${i + 1}`);
+    li.innerHTML = options[i];
+    ul.append(li);
+  }
+  return ul;
+};
+
+let rightMenu = ['Rename', 'Delete item'];
+
+rootNode.after(createMenu(rightMenu));
+
+const menu = document.querySelector('.right-click-menu')
+
+document.querySelectorAll('div').forEach((elem) => {
+  if (elem !== rootNode) {
+    elem.addEventListener('contextmenu', event => {
+      elem.style = 'background-color: lightgrey;'
+      event.preventDefault();
+      menu.style.top = `${event.clientY}px`;
+      menu.style.left = `${event.clientX}px`;
+      menu.classList.add('active');
+      menuUse(elem);
+    });
+  }
+});
+
+// Right-click-menu functions
+
+document.addEventListener('click', event => {
+  if (event.button === 0) {
+    menu.classList.remove('active');
+  }
+  document.querySelectorAll('div').forEach((elem) => {
+    elem.style = 'background-color: inherited;' 
+  })
+}, false);
+
+const menuUse = (target) => {
+  console.log(target)
+  document.querySelector('.opt1').addEventListener('click', () => {
+    alert('rename is forbidden');
+  }, false);
+  document.querySelector('.opt2').addEventListener('click', () => {
+    if (target.firstElementChild.classList.contains('file')){
+      target.remove();
+      return;
+    } else {
+    if (target.nextElementSibling.firstElementChild.classList.contains('empty_folder')) {
+      target.nextElementSibling.remove();
+    }
+    const childrenRemove = (targ) => {
+      const children = targ.parentElement.childElementCount;
+      for (let i = children - 1; i >= 0; i -= 1) {
+        targ.parentElement.children[i].remove();
+      }
+    }
+    childrenRemove(target);
+    target.remove();
+    }
+  }, false);
+};
+
 
